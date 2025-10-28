9d527b384681df0aed90dfe5050dbd146d19c4d4
Martin Thaller <martin.thaller@uipath.com>
2025-10-28 09:51:42 +0100

    dotbot

--- Create-SymLinks.ps1
+++
@@ -1,8 +0,0 @@
-$sourceDir = $PSScriptRoot
-
-Get-ChildItem "$sourceDir/crossplatform" |
-ForEach-Object {
-	$scriptName = $_.Name
-	New-Item -Type SymbolicLink -Path "$env:USERPROFILE/$scriptName" -Value "$sourceDir/crossplatform/$scriptName" -Force
-}
-
